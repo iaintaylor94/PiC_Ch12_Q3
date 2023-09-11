f029b116fe1b333f3826661d24d16d26b6f6dfd3
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-11 14:04:21 +0000

    completed program

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
+// Program to determine the number of bits in an int on the machine
+
 #include <stdio.h>
 
+// Declare Functions
+int int_size (void);
+
 int main(void) {
-  printf("Hello World\n");
+
+  printf ("The number of bits in an int on this machine is %d.\n", int_size());
+  
   return 0;
+}
+
+// Define Functions
+int int_size (void) {
+  unsigned int test = ~0; // All ones
+
+  int i = 0;
+  while (test != 0) {
+    test >>= 1;
+    i++;
+  }
+
+  return i;
 }
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Programming in C
+## Chapter 12
+### Question 3
+
+This problem is from chapter 12 of the textbook[^1]. This program determines the number of bits in an int on the machine.
+
+You may wish to run the program and compare its output.
+
+
+[^1]: Programming in C (Third edition) By Stephen G. Kochan - Page 297 : Question 3
